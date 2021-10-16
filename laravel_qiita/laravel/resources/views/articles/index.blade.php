54f0e4d6244b0d1ee7b591ed1d753c396d9fc9ec
harugramming <haruuchi.pool.0219@gmail.com>
2021-10-16 10:17:40 +0900

    記事更新と記事削除にポリシーの設定をしました。

--- laravel_qiita/laravel/resources/views/articles/index.blade.php
+++ laravel_qiita/laravel/resources/views/articles/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 @include('nav')
   <div class="container">
-    @foreach($articles as $article)@include('articles.card')
+    @foreach($articles as $article)
     @include('articles.card')
     @endforeach
   </div>
