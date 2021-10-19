66d6acb2c24515785a89e93760f40831d3f8dfc2
harugramming <haruuchi.pool.0219@gmail.com>
2021-10-19 21:45:24 +0900

    フォローでテーブル更新

--- laravel_qiita/laravel/app/User.php
+++ laravel_qiita/laravel/app/User.php
@@ -49,10 +49,44 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
     }
+
+    public function followings(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+    }
+
     public function isFollowedBy(?User $user): bool
     {
         return $user
             ? (bool)$this->followers->where('id', $user->id)->count()
             : false;
+    }
+    public function follow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id)
+        {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+        $request->user()->followings()->attach($user);
+
+        return ['name' => $name];
+    }
+
+    public function unfollow(Request $request, string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        if ($user->id === $request->user()->id)
+        {
+            return abort('404', 'Cannot follow yourself.');
+        }
+
+        $request->user()->followings()->detach($user);
+
+        return ['name' => $name];
     }
 }
